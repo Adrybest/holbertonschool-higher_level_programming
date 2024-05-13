87a87ac472573ddf707fbe90ee34de3f74680a51
Adrybest <adryanbestard@gmail.com>
2024-05-14 00:21:38 +0200

    5. Can you C me now?

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    rm =[mv for mv in my_string if mv != 'c' and mv != 'C']
+    rm = [mv for mv in my_string if mv != 'c' and mv != 'C']
     return ("".join(rm))
