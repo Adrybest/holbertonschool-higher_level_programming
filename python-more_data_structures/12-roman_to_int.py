ec7640e8551acce5456fcf29221b2b64f7b113d8
Adrybest <adryanbestard@gmail.com>
2024-05-17 14:45:09 +0200

    12. Roman to Integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,14 +4,14 @@ def roman_to_int(roman_string):
         return (0)
 
     roman_values = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-        }
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+    }
     res = 0
 
     for i in range(len(roman_string)):
