2b57a95743180f3d2230565fee5f670d564a391a
Adrybest <adryanbestard@gmail.com>
2024-06-19 14:26:55 +0200

    4. First table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+--  script that creates a table called first_table in the current database in your MySQL server
+-- 4-first_table.sql
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
