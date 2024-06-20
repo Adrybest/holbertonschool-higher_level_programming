5d9c77974646b5a3b68e6f7fd41a1edec7246fc9
Adrybest <adryanbestard@gmail.com>
2024-06-20 12:41:18 +0200

    10. List by best

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+-- 10-top_score.sql
+SELECT score, name FROM second_table ORDER BY score DESC;
