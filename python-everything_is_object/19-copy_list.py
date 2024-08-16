9efc801441d8df5e0075aec2352eba295d0781f7
Adrybest <adryanbestard@gmail.com>
2024-08-16 19:03:17 +0200

    add new line

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""function def copy_list(a_list): that returns a copy of a list"""
+
+
 def copy_list(l):
+    """function def copy_list(a_list): that returns a copy of a list"""
     return l.copy()
